a37bcb66df31c23aae650065f68d66ce4706e252
notapresent <notapresent@gmail.com>
2016-01-15 19:00:55 +0000

    First working version

---
+++ feed.php
@@ -0,0 +1,70 @@
+<?php
+require 'vendor/autoload.php';
+
+use PicoFeed\Reader\Reader;
+use PicoFeed\Syndication\Rss20;
+
+
+define('IMAGE_URL_RX', '/<img src="(http\:\/\/(?:[^"]+)\.(?:jpg|jpeg|gif|png))"\s*\/?/i');
+
+
+function extract_image_urls($desc) {
+    if(preg_match_all(IMAGE_URL_RX, $desc, $matches)) {
+        return $matches[1];
+    }
+
+    return array();
+}
+
+
+function url_to_filename($url) {
+    $parsed = parse_url($url);
+
+    if(!$parsed['path']) {
+        return false;
+    }
+
+    return basename($parsed['path']);
+}
+
+
+$url = urldecode($_GET['url']);
+
+$reader = new Reader;
+$resource = $reader->download($url);
+
+$parser = $reader->getParser(
+    $resource->getUrl(),
+    $resource->getContent(),
+    $resource->getEncoding()
+);
+
+$feed = $parser->execute();
+
+$image_items = array();
+
+foreach($feed->items as $item) {
+
+    $image_urls = extract_image_urls($item->content);
+
+    foreach($image_urls as $url) {
+        $image_items[] = array(
+            'title' => url_to_filename($url),
+            'updated' => $item->date->getTimestamp(),
+            'url' => $url,
+            'summary' => "<img src=\"{$url}\">",
+        );
+    }
+}
+
+
+$writer = new Rss20();
+$writer->title = $feed->title;
+$writer->site_url = $feed->site_url;
+$writer->feed_url = "http://{$_SERVER['HTTP_HOST']}/feed.php?url=" . urlencode($url);
+$writer->description = "{$feed->description}\nModified by Splitch (http://{$_SERVER['HTTP_HOST']})";
+
+$writer->items = $image_items;
+
+header('content-type: application/rss+xml');
+echo $writer->execute();
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Splitch</title>
+    </head>
+    <body>
+        <form method=get action=feed.php>
+            <label for=url>URL
+                <input id=url type=text name=url value="http://conceptships.blogspot.com/feeds/posts/default?alt=rss">
+            </label>
+
+            <input type=submit value=Submit>
+        </form>
+    </body>
+</html>
