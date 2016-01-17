1f580e0b8c9844be5129165de28f1f4a438180a4
notapresent <notapresent@gmail.com>
2016-01-17 22:56:16 +0000

    Code cleanup

--- index.php
+++ index.php
@@ -56,7 +56,6 @@
     </div>
 
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
     <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="static/ie10-viewport-bug-workaround.js"></script>
   </body>
